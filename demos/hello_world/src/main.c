7700e4ba43edfc4ac455bd5b97d72c6ad5714f6d
Yong Zhou <zyong06@gmail.com>
2024-04-10 11:45:17 +0800

    [refactor] dedicated directory for demo apps

--- demos/hello_world/src/main.c
+++ demos/hello_world/src/main.c
@@ -4,7 +4,7 @@
 int main(void) {
   int i = 0;
   while (1) {
-    printf("%d: Demo Application on %s\n", i++, CONFIG_BOARD);
+    printf("%d: demo/hello_world app on %s\n", i++, CONFIG_BOARD);
     k_msleep(2000);
   }
   return 0;
