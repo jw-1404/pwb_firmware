2c1367dc5f58de2c3565486e18646c37e8a6adaf
Zhou <zyong06@gmail.com>
2025-06-11 10:35:06 +0800

    [modify] braud rate to 9600

--- applications/lv_app/src/modbus.c
+++ applications/lv_app/src/modbus.c
@@ -117,7 +117,7 @@ const static struct modbus_iface_param server_param = {
     .serial =
         {
             /* .baud = 19200, */
-            .baud = 115200,
+            .baud = 9600,
             .parity = UART_CFG_PARITY_NONE,
         },
 };
