5b6a5037b507cfbdfcaea26b44ccef88cc6585c8
Zhou <zyong06@gmail.com>
2025-10-21 12:08:05 +0800

    change braudrate to 19200

--- applications/lv_app/src/modbus.c
+++ applications/lv_app/src/modbus.c
@@ -116,8 +116,7 @@ const static struct modbus_iface_param server_param = {
         },
     .serial =
         {
-            /* .baud = 19200, */
-            .baud = 9600,
+            .baud = 19200,
             .parity = UART_CFG_PARITY_NONE,
         },
 };
